8907d14512f03cf5a5bd493b5e93cb2ffb2a68a9
Ryan Andersen <ryan.andersen05@gmail.com>
2017-12-09 15:36:33 -0500

    Added some methods to player stats

--- Assets/Scripts/CharacterStats/PlayerStats.cs
+++ Assets/Scripts/CharacterStats/PlayerStats.cs
@@ -1,22 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerStats : MonoBehaviour {
+    #region main variables 
     [Tooltip("The maximum health that a player can be at any given point. Normally health will not go beyond this after collecting health packs")]
     public float maxHealth = 100;
 
 
     public CharacterMovement characterMovement { get; private set; }
     public PlayerController playerController { get; private set; }
-	// Use this for initialization
-	void Start () {
+
+    private float currentHealth;
+    #endregion main variables
+
+
+    #region monobehaviour methods
+    private void Awake()
+    {
+        this.currentHealth = maxHealth;
         this.characterMovement = GetComponent<CharacterMovement>();
         this.playerController = GetComponent<PlayerController>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+    #endregion monobehaviour methods
+
+
+    #region health methods
+    public void AddHealth(float healthToAdd)
+    {
+        this.currentHealth += healthToAdd;
+        this.currentHealth = Mathf.Min(this.currentHealth, this.maxHealth);
+
+    }
+
+    public void TakeDamage(float damageToTake)
+    {
+        this.currentHealth -= damageToTake;
+        if (currentHealth <= 0)
+        {
+            OnCharacterDead();
+        }
+        this.currentHealth = Mathf.Max(0, this.currentHealth);
+    }
+
+    /// <summary>
+    /// This method will be called when a character has taken damage and its health has fallen below 0.
+    /// </summary>
+    public void OnCharacterDead()
+    {
+
+    }
+    #endregion health methods
 }
