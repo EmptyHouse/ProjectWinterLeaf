f9fc71ad55c3027443dd7fc2f1d524e2c3ee9db0
Ryan Andersen <ryan.andersen05@gmail.com>
2017-12-08 21:57:39 -0500

    Created a player prefab

--- Assets/Scripts/GameMechanics/CharacterMovement.cs
+++ Assets/Scripts/GameMechanics/CharacterMovement.cs
@@ -3,5 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
+    #region main variables
+    public float walkSpeed = 15;
+    public float runSpeed = 25;
+
+    public float acceleration = 15;
+
+
+    private Animator anim;
+    #endregion main variables
+
+    #region monobehaviour methods
+
+    private void Awake()
+    {
+        this.anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+
+    }
+    #endregion monobehaviour methods
 
 }
