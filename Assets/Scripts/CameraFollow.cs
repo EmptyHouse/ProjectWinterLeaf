dc2d7f3facefc62494d7b1a7e3471a71d93ed2a4
Ryan Andersen <ryan.andersen05@gmail.com>
2017-12-08 21:48:46 -0500

    Added some placeholder methods to CameraFollow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
+    #region main variables
     [Tooltip("A reference to the player to follow. This is the most important unit to follow when calculating camera position")]
     private Transform playerTransform;
+    [Tooltip("This list will hold enemy Transforms that the camera shold focus. Typically when in action. Can also be used to focus on important items")]
+    private List<Transform> secondaryTargetList = new List<Transform>();
+    #endregion main variables
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    #region monobehaviour methods
+    private void Awake()
+    {
+        
+    }
+
+    /// <summary>
+    /// Camera should be one of the last things to update, so that the character and enemies have time to update
+    /// their positions before updateing the camera's position
+    /// </summary>
+    private void LateUpdate()
+    {
+        
+    }
+    #endregion monobehaviour methods
+
+    public void AddSecondaryTargetToFollow(Transform transformToAdd)
+    {
+
+    }
+
+    public void RemoveSecondaryTargetToFollow(Transform trasformToRemove)
+    {
+
+    }
 }
--- Assets/Scripts/GameMechanics/CharacterMovement.cs
+++ Assets/Scripts/GameMechanics/CharacterMovement.cs
@@ -4,13 +4,4 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
