e6594711635bb361993633ff586ad90cd14c8b74
Ryan Andersen <ryan.andersen05@gmail.com>
2017-12-08 21:39:49 -0500

    Scripts and player placeholder added

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    [Tooltip("A reference to the player to follow. This is the most important unit to follow when calculating camera position")]
+    private Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
