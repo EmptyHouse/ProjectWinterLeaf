bbe65694383bc6bea70c0b740a5daf41935774d5
Ryan Andersen <ryan.andersen05@gmail.com>
2017-12-13 22:05:59 -0500

    In game ui started

---
+++ Assets/Scripts/UIScripts/InGameUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameUI : MonoBehaviour {
+    #region static variables
+    private InGameUI instance;
+    
+    public InGameUI Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<InGameUI>();
+            }
+            return instance;
+        }
+    }
+    #endregion static variables
+
+    #region monobehaviour methods
+    private void Awake()
+    {
+        
+    }
+    #endregion monobehaviour methods
+
+}
---
+++ Assets/Scripts/UIScripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    #region main variables
+    public Transform pauseScreen;
+    #endregion main variables
+
+
+    #region monobehaviour methods
+    private void Awake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+    #endregion monobehaviour methods
+
+
+    #region option events
+    public void OnResumeButtonPressed()
+    {
+
+    }
+
+    public void OnExitGamePressed()
+    {
+
+    }
+
+    #endregion option events
+}
